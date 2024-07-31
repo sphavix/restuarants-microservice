20753b82f260c33ad2ce4c719c90dfd60c5ae417
Siphamandla Zolakhe <zwelakhespha@gmail.com>
2024-07-31 22:21:06 +0200

    Added Entities and App Modules

---
+++ Restuarants.Domain/Entities/Dish.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restuarants.Domain.Entities
+{
+    public class Dish
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+    }
+}
---
+++ Restuarants.Domain/Entities/Restuarant.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restuarants.Domain.Entities
+{
+    public class Restuarant
+    {
+        public string Name { get; set; } = default!;
+        public string Description { get; set; } = default!;
+        public string Category { get; set; } = default!;
+        public bool HasDelivery { get; set; }
+
+        public string? ContactEmail { get; set; }
+        public string? ContactNumber { get; set; }
+
+        public Address? Address { get; set; }
+        public List<Dish> Dishes { get; set; } = new();
+    }
+}
